6e1e76388874eaea8bbc802b6eaa319580df3da6
JeonghoPark <pjhpjh0805@naver.com>
2023-06-14 22:14:45 +0900

    feat: elasticConfig.class

---
+++ src/main/java/com/example/ElasticSearch/config/ElasticSearchConfig.java
@@ -0,0 +1,31 @@
+package com.example.ElasticSearch.config;
+
+import com.example.ElasticSearch.repository.MemberRepository;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackageClasses = MemberRepository.class)
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchOperations() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }
+}
