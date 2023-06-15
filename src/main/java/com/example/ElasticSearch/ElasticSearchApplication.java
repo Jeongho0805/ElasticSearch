38e06be6fb548e137b4441481331242930266567
JeonghoPark <pjhpjh0805@naver.com>
2023-06-15 11:50:10 +0900

    fix: ElasticSearchApplication

--- src/main/java/com/example/ElasticSearch/ElasticSearchApplication.java
+++ src/main/java/com/example/ElasticSearch/ElasticSearchApplication.java
@@ -1,13 +1,18 @@
 package com.example.ElasticSearch;
 
+import com.example.ElasticSearch.repository.RecruitmentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = RecruitmentRepository.class))
 @SpringBootApplication
 public class ElasticSearchApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticSearchApplication.class, args);
 	}
-
 }
